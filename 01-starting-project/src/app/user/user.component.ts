399f6fefed12c5d22c05681f4577a32f08d5af78
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-16 18:54:42 -0500

    added property binding

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'; // importing DUMMY_USERS for the UserComponent.
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) // allows us to get a random number between zero and the highest available index in the array.
 
 @Component({
   selector: 'app-user',
@@ -7,6 +10,8 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+// For the DUMMY_USERS; allows us to draw a user at random.
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]; // Output: random id chosen from zero to highest index. We can now access selectedUser from inside the template of this component (all properties in this class are available in the templateUrl).
 }
