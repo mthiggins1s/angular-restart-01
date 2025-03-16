f26412bae080f467fe6c4a09a41812eda0c5bdac
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-16 16:40:42 -0500

    added dummy users

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
+  standalone: true,
   imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
