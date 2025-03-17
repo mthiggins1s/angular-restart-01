3c8be8b22bf0e0750fc285f69352ad95aba42ee8
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-17 15:21:39 -0500

    made changes

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
@@ -7,7 +9,7 @@ import { TasksComponent } from "./tasks/tasks.component"; // we expose the impor
 @Component({ // decorator; TypeScipt feature to add metadata (comes from the Component in the Angular package).
   selector: 'app-root', // tells Angular what element it should look for so they can be replaced by this component and its markup (markup is stored in the templateUrl).
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor],
   templateUrl: './app.component.html', // contains the markup for the html element.
   styleUrl: './app.component.css', // contains the markup for the css element.
 })
