c7e74923fcbb9038a7de3597bd524e8cc4454027
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-17 00:34:02 -0500

    starting Signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -14,4 +14,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) // allows us 
 // For the DUMMY_USERS; allows us to draw a user at random.
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex]; // Output: random id chosen from zero to highest index. We can now access selectedUser from inside the template of this component (all properties in this class are available in the templateUrl).
+
+  get imagePath() { // this makes a getter; a method (function inside a class), useable like a property (it doesnt need to be called explicitly and returns a new value)
+    return 'assets/users/' + this.selectedUser.avatar // we have to add 'this.' because we are doing this from INSIDE THE CLASS INSTEAD OF INSIDE THE TEMPLATE.
+  }
+
+  onSelectUser() {  // this gets executed everytime we click on the user.
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
