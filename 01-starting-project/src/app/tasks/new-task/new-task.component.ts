441615584cf6df8c861317d84f7c2d6bb8feb239
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-21 07:13:22 -0500

    completed two-way binding

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule], // FormsModule is a collection of directives and features (for NgModel)
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>()
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
   
   onCancel() {
     this.cancel.emit();
